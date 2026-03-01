76b61afdf5f32e3daae0f26b54abe320a898e94d
Avinash <avinash592004@gmail.com>
2026-03-01 14:15:42 +0530

    test model

---
+++ model.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SimpleNet(nn.Module):
+    """
+    A simple neural network with one hidden layer.
+    """
+    def __init__(self):
+        super(SimpleNet, self).__init__()
+        self.fc1 = nn.Linear(10, 20)
+        self.fc2 = nn.Linear(20, 2)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
